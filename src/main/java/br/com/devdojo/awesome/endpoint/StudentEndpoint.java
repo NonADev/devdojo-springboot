e2d855f9e63268c3daabedb43ccfc6a91db9a393
nonadev <wesley.vieira.lima.1999@gmail.com>
2019-08-21 13:16:29 -0300

    adicionado builder retornando o proprio objeto

--- src/main/java/br/com/devdojo/awesome/endpoint/StudentEndpoint.java
+++ src/main/java/br/com/devdojo/awesome/endpoint/StudentEndpoint.java
@@ -1,12 +1,16 @@
 package br.com.devdojo.awesome.endpoint;
 
 import br.com.devdojo.awesome.error.CustomErrorType;
+import br.com.devdojo.awesome.error.ResourceNotFoundException;
 import br.com.devdojo.awesome.model.Student;
 import br.com.devdojo.awesome.repository.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("students")
@@ -23,28 +27,42 @@ public class StudentEndpoint {
         return new ResponseEntity<>(studentDAO.findAll(), HttpStatus.OK);
     }
 
-    @GetMapping(path = "/{id}")
-    public ResponseEntity<?> getStudentById(@PathVariable("id") Long search_id) {
-        Student student = studentDAO.findOne(search_id);
-        if (student == null)
-            return new ResponseEntity<>(new CustomErrorType("Student Not Found"), HttpStatus.NOT_FOUND);
+    @GetMapping(path = "/findById/{id}")
+    public ResponseEntity<?> getStudentById(@PathVariable("id") Long pk_id) {
+        Student student = studentDAO.findOne(pk_id);
+        verifyIfStudentExists(pk_id);
         return new ResponseEntity<>(student, HttpStatus.OK);
     }
 
-    @PostMapping
-    public ResponseEntity<?> save(@RequestBody Student student) {
-        return new ResponseEntity<>(studentDAO.save(student), HttpStatus.OK);
+    @GetMapping(path = "/findByName/{name}")
+    public ResponseEntity<?> getStudentByName(@PathVariable("name") String name) {
+        if (studentDAO.findByName(name) == null)
+            throw new ResourceNotFoundException("Student not found for name: " + name);
+        return new ResponseEntity<>(studentDAO.findByName(name), HttpStatus.OK);
     }
 
-    @DeleteMapping(path="/{pk_id}")
+    @PostMapping
+    @Transactional()
+    public ResponseEntity<?> save(@Valid @RequestBody Student student) {
+        return new ResponseEntity<>(studentDAO.save(student), HttpStatus.CREATED);
+    }
+
+    @DeleteMapping(path = "/{pk_id}")
     public ResponseEntity<?> delete(@PathVariable Long pk_id) {
+        verifyIfStudentExists(pk_id);
         studentDAO.delete(pk_id);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
     @PutMapping
     public ResponseEntity<?> update(@RequestBody Student student) {
+        verifyIfStudentExists(student.getPk_id());
         studentDAO.save(student);
         return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    private void verifyIfStudentExists(Long pk_id) {
+        if (studentDAO.findOne(pk_id) == null)
+            throw new ResourceNotFoundException("Student not found for ID: " + pk_id);
     }
 }
--- src/main/java/br/com/devdojo/awesome/model/Student.java
+++ src/main/java/br/com/devdojo/awesome/model/Student.java
@@ -1,10 +1,25 @@
 package br.com.devdojo.awesome.model;
 
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.NotEmpty;
+
 import javax.persistence.Entity;
 
 @Entity
 public class Student extends AbstractEntity{
+    @NotEmpty
     private String name;
+
+    @Email
+    private String email;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
     public String getName() {
         return name;
